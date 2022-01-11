305cac25e104a9c092ce72950176598c83d6eeb4
BSMagway <bsmagway@gmail.com>
2022-01-11 20:52:54 +0300

    Homework (Lesson 01)

--- WpfITStep/MainWindow.xaml.cs
+++ WpfITStep/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfITStep
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Something happened", "Result");
+        }
     }
 }
